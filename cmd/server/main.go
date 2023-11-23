1d566b9a7f0b684863793bcbcba092094e12623d
kzuchowskiobjectivity <kzuchowski@objectivity.co.uk>
2023-11-23 16:08:24 +0100

    Handle server run error

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -35,5 +35,8 @@ func main() {
 	ihttp.Routes(&r.RouterGroup, handler)
 
 	portNumber := viper.GetString("port")
-	r.Run(portNumber)
+	runErr := r.Run(portNumber)
+	if runErr != nil {
+		log.Fatal(err)
+	}
 }
